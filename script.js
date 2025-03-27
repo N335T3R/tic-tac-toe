1b16364b885d69162e503c9d1ba70da89e1fae0b
N335T3R <n335t3r@gmail.com>
2025-03-27 17:29:25 -0400

    Board methods added: initBoard, selectCell, isCellEligible, placeMarker

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
+// IIFEs
 const board = (function() {
     return {
-        board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        board: [],
+        occupiedCells: [],
+        initBoard: function(div) {
+            for (let i = 0; i < 9; i++) {
+                const cell = document.createElement('div');
+                const markerTextBox = document.createElement('p');
+                cell.classList.add('cell');
+                markerTextBox.classList.add('marker-text');
+                cell.appendChild(markerTextBox);
+                div.appendChild(cell);
+                this.board.push(cell);
+            }
+        },
+        selectCell: function(board) {
+            this.board.forEach(cell => {
+                cell.addEventListener('click', () => {
+                    // return index of selected cell
+                    // remove eventListeners
+                    return cell;
+                });
+            });
+        },
+        isCellEligible: function (cell) {
+            if (this.occupiedCells.includes(cell)) return false;
+            else return cell;
+        },
+        // accepts return value of
+        // this.isCellEligible() if return !== false
+        placeMarker: function (cell, marker) {
+            const ps = Array.from(document.getElementsByClassName('marker-text'));
+            ps[this.board.indexOf(cell)].innerText = `${marker}`;
+            this.occupiedCells.push(cell);
+            console.log(this.occupiedCells);
+        }
     }
 })();
 
+const game = (function(board,) {
+    this.board = board;
+})();
 
+
+
+board.initBoard(document.getElementById('board'));
+const player1 = createPlayer('anthony', 'x');
+board.selectCell();
+console.log(player1, board);
+
+
+// Functions & Factories
 function createPlayer(name, marker) {
-    return { name, marker };
+    return { name, 
+        marker,};
 }
 
-const player1 = createPlayer('anthony', 'x');
-console.log(player1, board);
