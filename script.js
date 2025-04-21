1c1a3b911bad4b623f6254f08116e1c4fe1a31de
N335T3R <n335t3r@gmail.com>
2025-04-21 17:55:09 -0400

    Game plays in console, but event listener needs to be removed on win
    Player marks appear on board

--- script.js
+++ script.js
@@ -1,107 +1,112 @@
-// IIFEs
+// player
+function createPlayer(name, marker) {
+    return { name,
+        marker,
+        occupiedCells: [],
+    };
+}
+
+
+
+// board
 const board = (function() {
     return {
-        board: [],
+        cells: [],
         occupiedCells: [],
         initBoard: function(div) {
             for (let i = 0; i < 9; i++) {
                 const cell = document.createElement('div');
                 const markerTextBox = document.createElement('p');
-                cell.classList.add('cell');
+
+                cell.classList.add('cell', `class${i + 1}`);
                 markerTextBox.classList.add('marker-text');
+                
                 cell.appendChild(markerTextBox);
                 div.appendChild(cell);
-                this.board.push(cell);
+                this.cells.push(cell);
             }
         },
-        handleClick: function(event) {
-            const cell = event.target;
-
-            this.board.forEach(square => {
-                square.removeEventListener('click', this.boundHandleClick);
-            });
-
-            this.isCellEligible(cell);
-        },
-        selectCell: function() {
-            // must bind "this" to current object,
-            // otherwise when eventListener is added,
-            // "this" will become event object
-            this.boundHandleClick = this.handleClick.bind(this);
-
-            this.board.forEach(square => {
-                square.addEventListener('click', this.boundHandleClick);
-            });
-        },
-        isCellEligible: function (cell) {
-
-            if (this.occupiedCells.includes(cell)) {
-                console.log('Cell already captured. Choose again.');
-                this.selectCell();
-            }
-            else this.placeMarker(cell);
-        },
-        // accepts return value of
-        // this.isCellEligible() if return !== false
-        placeMarker: function (cell) {
-            // GET CURRENT PLAYER
-            // const player = game.currentPlayer;
-            const player = player1;
-
-            // if cell === false, this.selectCell()
+        placeMarker: function(cell, player) {
             const ps = Array.from(document.getElementsByClassName('marker-text'));
-            ps[this.board.indexOf(cell)].innerText = `${player.marker}`;
-
-            this.occupiedCells.push(cell);
-
-            return cell;
-            // Return value?
-            // Add argument "player"?
-            // push eligible cell to player.occupiedCells?
-        },
+            ps[this.cells.indexOf(cell)].innerText = `${player.marker}`;
+        }
     }
 })();
 
 
 
-// Functions & Factories
-function createPlayer(name, marker) {
-    return { name,
-        marker,
-        occupiedCells: []
-    };
-}
-
-function createGame(board, player1, player2) {
+// ref
+function createRef(player1, player2, board) {
     return {
-        board,
         player1,
         player2,
-        takeTurn: function(player) {
-            // alert('Choose a cell');
+        board,
+        turns: 1,
+        wins: [
+            [1, 2, 3],
+            [1, 5, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 5, 7],
+            [3, 6, 9],
+            [4, 5, 6],
+            [7, 8, 9]
+        ],
+        getInd: function(cell) {
+            let str = cell.classList[1];
+            
+            return Number(str[5]);
+        },
+        selectCell: function(e) {
+            let player;
+            const cell = e.target;
+            const ind = this.getInd(cell);
 
-            const cell = this.board.selectCell(player);
-            console.log(cell);
+            // determine player
+            if (this.turns % 2 !== 0) player = player1;
+            else player = player2;
+
+            player.occupiedCells.push(ind);
+            this.board.placeMarker(cell, player);
+
+            this.turns++;
+            console.log(cell, ind, player);
+
+            let win = this.checkWin(player);
+
+            if (win === true) this.declareWin(player);
+            else return 0;
         },
         checkWin: function(player) {
-            // Check for a win after each turn
+            let cells = player.occupiedCells;
+
+            console.log(cells);
+            return this.wins.some(win => 
+                win.every(cell => cells.includes(cell)));
+        },
+        declareWin: function(player) {
+            // board highlights winning squares
+            // referee text area congrats winner
+            console.log(`${player.name} wins!`);
+            // OR
+
+            // modal appears & contains congrats
+            // to winner & restart button
         }
     }
 }
 
 
-
-
-// Execution
+// execution
 const player1 = createPlayer('anthony', 'x');
-const player2 = createPlayer('RJ', "O");
-board.initBoard(document.getElementById('board'));
-const game = createGame(board, player1, player2);
+const player2 = createPlayer('RJ', "o");
+const ref = createRef(player1, player2, board)
 
-game.takeTurn(player1);
+function playGame(ref) {
+    ref.board.initBoard(document.getElementById('board'));
 
-console.log(player1, board, game);
-
-
-
-
+    ref.board.cells.forEach(cell => {
+        cell.addEventListener('click', ref.selectCell.bind(ref), { once: true });
+    });
+}
+playGame(ref);
